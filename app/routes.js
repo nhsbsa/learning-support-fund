6711d46b19bc33de6a98616908f3c0bbe299130f
Dean Cobb <deaco@bsa-mb-vd4nc9rxjv.home>
2023-02-02 11:45:59 +0000

    include routes partial v3

--- app/routes.js
+++ app/routes.js
@@ -21,6 +21,7 @@ router.use('/lsf-portal/v1', require('./views/lsf-portal/v1/_routes'));
 // ================================
 // Routes for LSF Public
 // ================================
+router.use('/lsf-public/v3', require('./views/lsf-public/v3/_routes'));
 router.use('/lsf-public/v2', require('./views/lsf-public/v2/_routes'));
 router.use('/lsf-public/v1', require('./views/lsf-public/v1/_routes'));
 
