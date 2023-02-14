be59bdc5f3d3cba8d7de3be14569749e85f70fd5
Dean Cobb <dean.cobb@nhs.net>
2023-02-14 14:07:20 +0000

    claims list filter

--- app/assets/javascript/main.js
+++ app/assets/javascript/main.js
@@ -1,1 +1,15 @@
 // ES6 or Vanilla JavaScript
+document.getElementById('toggle-filter').addEventListener('click', () => {
+
+    document.getElementById('filter-column').classList.toggle('nhsbsa-hide-filter')
+    document.getElementById('table-column').classList.toggle('nhsuk-grid-column-three-quarters')
+    document.getElementById('table-column').classList.toggle('nhsuk-grid-column-full')
+
+
+    if (document.getElementById("toggle-filter").innerHTML === "Show filter") {
+        document.getElementById("toggle-filter").innerHTML = "Hide filter";
+    } else {
+        document.getElementById("toggle-filter").innerHTML = "Show filter";
+    }
+
+});
