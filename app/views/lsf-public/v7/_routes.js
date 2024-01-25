5620f6d00c7b402da6a76957b130f9b46c7de3c3
Joe Hutchinson NHSBSA <55741037+joe-hutchinson@users.noreply.github.com>
2024-01-25 09:03:43 +0000

    removed TDAE-accommodation-journey from scenario

--- app/views/lsf-public/v7/_routes.js
+++ app/views/lsf-public/v7/_routes.js
@@ -1390,8 +1390,6 @@ router.post('/v7/TDAE-travel-accommodation', (req, res) => {
         res.redirect('/lsf-public/v7/TDAE-signpost/TDAE-alternative-journey-signpost')
     } else if (change == ('yes')) {
         res.redirect('/lsf-public/v7/TDAE-eligibility-cya')
-    } else if (claimingFor.includes('accommodation') || claimingFor.includes('car') || claimingFor.includes('Public transport')) {
-        res.redirect('/lsf-public/v7/TDAE-accommodation-journey')
     } else {
         res.redirect('/lsf-public/v7/TDAE-reuse-answers')
     }
